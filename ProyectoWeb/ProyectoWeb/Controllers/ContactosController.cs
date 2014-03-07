344030b9e0cad30ca86a348bb891dee258a5b571
efrojasm <efrojasm@gmail.com>
2014-03-07 12:02:18 -0600

    casi completo
    envio de correos listo!!!! solo falta publicarlo

--- ProyectoWeb/ProyectoWeb/Controllers/ContactosController.cs
+++ ProyectoWeb/ProyectoWeb/Controllers/ContactosController.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ProyectoWeb.Models;
-using System.Net.Mail;
-using System.Net;
 
 namespace ProyectoWeb.Controllers
 {
@@ -20,52 +18,21 @@ namespace ProyectoWeb.Controllers
             List<Contacto> Contactos = db.Contactos.ToList();
             ViewBag.contactos = Contactos;
             return View();
-
         }
-
-        public static void RetryIfBusy(string correo="",string mensaje="",string nombre="")
+        public ActionResult Email()
         {
-            MailAddress from = new MailAddress(correo);
-            MailAddress to = new MailAddress("efrojasm@gmail.com");
-            MailMessage message = new MailMessage(from, to);
-            // message.Subject = "Using the SmtpClient class.";
-            message.Subject = "Contacto";
-            message.Body = @nombre+"\n"+mensaje;
-            // Add a carbon copy recipient.
-            SmtpClient client = new SmtpClient("smtp.gmail.com");
-            // Include credentials if the server requires them.
-            Console.WriteLine("Sending an e-mail message to {0} using the SMTP host {1}.",
-                 to.Address, client.Host);
-            try
-            {
-                client.Send(message);
-            }
-            catch (SmtpFailedRecipientsException ex)
-            {
-                for (int i = 0; i < ex.InnerExceptions.Length; i++)
-                {
-                    SmtpStatusCode status = ex.InnerExceptions[i].StatusCode;
-                    if (status == SmtpStatusCode.MailboxBusy ||
-                        status == SmtpStatusCode.MailboxUnavailable)
-                    {
-                        Console.WriteLine("Delivery failed - retrying in 5 seconds.");
-                        System.Threading.Thread.Sleep(5000);
-                        client.Send(message);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Failed to deliver message to {0}",
-                            ex.InnerExceptions[i].FailedRecipient);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Exception caught in RetryIfBusy(): {0}",
-                        ex.ToString());
-            }
+            return View();
         }
+        public ActionResult SendMail(string Subject, string Body, string Mail)
+        {
 
+
+            EMail oMail = new EMail();
+         
+            oMail.SendMail("Email", Mail, new String[] { Subject, Body });
+            
+            return Content("Enviado");
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -74,7 +41,8 @@ namespace ProyectoWeb.Controllers
 
         }
 
-
-
     }
 }
+
+
+
